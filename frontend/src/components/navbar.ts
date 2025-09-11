311d22e0f2a0320efbee51dd3cb0e4254ab0b465
yuhe-02 <domineter4649@gmail.com>
2025-09-11 14:02:18 +0900

    [FIX] divide global and local css

--- frontend/src/components/navbar.ts
+++ frontend/src/components/navbar.ts
@@ -2,15 +2,11 @@ import { routes } from "@/router/routes";
 import { navigate } from "@/router/router";
 import "./navbar.css";
 
-export function renderNavbar(classNames: string[] = ["nav-link"]): HTMLElement {
-  const nav = document.createElement("nav");
-  nav.classList.add(
-    "flex",
-    "justify-around",
-    "bg-gray-800",
-    "text-white",
-    "p-4",
-  );
+// リンク要素の DocumentFragment を返す
+export function renderNavbar(
+  classNames: string[] = ["nav-link"],
+): DocumentFragment {
+  const fragment = document.createDocumentFragment();
 
   Object.keys(routes).forEach((route) => {
     const { linkLabel } = routes[route];
@@ -19,13 +15,14 @@ export function renderNavbar(classNames: string[] = ["nav-link"]): HTMLElement {
     linkElement.href = route;
     linkElement.textContent = linkLabel;
     linkElement.classList.add(...classNames);
+
     linkElement.addEventListener("click", (e) => {
       e.preventDefault();
       navigate(route);
     });
 
-    nav.appendChild(linkElement);
+    fragment.appendChild(linkElement);
   });
 
-  return nav;
+  return fragment;
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,16 +3,27 @@ import {
   renderInitialPage,
   registerBrowserBackAndForth,
 } from "@/router/router";
-
-const navbar = renderNavbar([
-  "nav-link",
-  "text-white",
-  "hover:text-yellow-300",
-  "px-4",
-]);
+import "./style.css";
 
 const navContainer = document.querySelector("#nav")!;
-navContainer.appendChild(navbar);
+navContainer.classList.add(
+  "flex",
+  "gap-4",
+  "fixed",
+  "top-0",
+  "left-0",
+  "w-full",
+  "bg-gray-800",
+  "text-white",
+  "p-2",
+  "box-border",
+  "mb-4",
+);
+
+// 中にリンクを追加
+navContainer.appendChild(
+  renderNavbar(["nav-link", "hover:text-yellow-300", "px-4"]),
+);
 
 registerBrowserBackAndForth();
 renderInitialPage();
