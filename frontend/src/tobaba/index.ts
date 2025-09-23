e5a3c0e040ba0d6aa58c41a7bad64ad03e595840
17931837B <tomoya.bb1014@gmail.com>
2025-09-23 14:09:02 +0900

    [fix] カウントダウン中でもポーズ画面へ移行できるよう修正

--- frontend/src/tobaba/index.ts
+++ frontend/src/tobaba/index.ts
@@ -126,7 +126,7 @@ function handlePlayerMovement(deltaTime: number) {
 document.addEventListener('keydown', (e) => {
     gameData.keysPressed[e.key] = true;
     if (e.key.toLowerCase() === 'p') {
-        if (gameData.gameState === 'game') {
+        if (gameData.gameState === 'game' || gameData.gameState === 'countingDown') {
             setGameState('paused');
         } else if (gameData.gameState === 'paused') {
             setGameState('game');
