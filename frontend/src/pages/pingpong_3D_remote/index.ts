f3edc10422f9ed9cb23ec2612348d49a22b5b2ae
17931837B <tomoya.bb1014@gmail.com>
2026-01-21 10:32:43 +0900

    [fix] icon配置変更

--- frontend/src/pages/pingpong_3D_remote/index.ts
+++ frontend/src/pages/pingpong_3D_remote/index.ts
@@ -8,13 +8,8 @@ import {
 } from "@/utils/pingpong3D/remoteSetting";
 import { t, word } from "@/i18n";
 import "./style.css";
+import { COPY_ICON } from "../../../public/icons";
 
-const COPY_ICON: string = `
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-</svg>
-`.trim();
 const POLLING_INTERVAL_MS = 1500;
 const FEEDBACK_DISPLAY_MS = 1500;
 
