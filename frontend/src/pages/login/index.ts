3865edf0b28d10f0c69696d7ecec3bfdc53edbe0
Yosshii-42 <yoshi.tsurudome@gmail.com>
2026-02-19 00:02:49 +0900

    tab fix

--- frontend/src/pages/login/index.ts
+++ frontend/src/pages/login/index.ts
@@ -19,143 +19,145 @@ const API_BASE = "/api/common";
 class LoginComponent implements Component {
   render = () => {
     return `
-						<div class="
-									min-h-[calc(100vh-64)]
-									flex items-center justify-center
-									bg-[radial-gradient(circle_at_center,#1c1c1c,#0b0b0b)]
-								 ">
-							<div class="
-										w-[380px]	p-8
-										border-2 border-slate-500 rounded-2xl
-										shadow-[0_0_30px_rgba(0,0,0,0.8)]
-									 ">
-								<h2 class="
-											text-center text-slate-50 text-5xl
-											font-bold tracking-[0.1em]
-											mb-7
-										">${t("login")}
-								</h2>
+            <div class="
+                  min-h-[calc(100vh-64px)]
+                  flex items-center justify-center
+                  bg-[radial-gradient(circle_at_center,#1c1c1c,#0b0b0b)]
+                ">
+              <div class="
+                    w-[380px]	p-8
+                    border-2 border-slate-500 rounded-2xl
+                    shadow-[0_0_30px_rgba(0,0,0,0.8)]
+                   ">
+                <h2 class="
+                      text-center text-slate-50 text-5xl
+                      font-bold tracking-[0.1em]
+                      mb-7
+                    ">
+                  ${t("login")}
+                </h2>
 
-								<form id="login-form" class="accent-slate-800">
+                <form id="login-form" class="accent-slate-800">
 
-									<!-- Email -->
-									<div class="accent-slate-800">
-										<label for="email"
-														class="
-															block mb-1 text-sm
-															font-bold tracking-[0.1em]
-															text-slate-200
-														">${t("email")}
-										</label>
-										<input
-											type="email"
-											id="email"
-											name="email"
-											placeholder="you@example.com"
-											required
-											class="
-												w-full px-2 py-3 mb-5
-												bg-slate-700
-												border border-slate-500 rounded-md
-												text-slate-200 text-base
-												leading-normal
-											"
-										/>
-									</div>
+                  <!-- Email -->
+                  <div class="accent-slate-800">
+                    <label for="email"
+                           class="
+                            block mb-1 text-sm
+                            font-bold tracking-[0.1em]
+                            text-slate-200
+                           ">
+                      ${t("email")}
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="you@example.com"
+                      required
+                      class="
+                      w-full px-2 py-3 mb-5
+                        bg-slate-700
+                        border border-slate-500 rounded-md
+                        text-slate-200 text-base
+                        leading-normal
+                      "
+                    />
+                  </div>
 
-									<!-- Password -->
-									<div class="accent-slate-800">
-										<label for="password"
-													class="
-															block mb-1 text-sm
-															font-bold tracking-[0.1em]
-															text-slate-200
-													">
-											${t("password")}
-										</label>
-										<input
-											type="password"
-											id="password"
-											name="password"
-											placeholder="••••••••"
-											required
-											class="
-													w-full px-2 py-3 mb-5
-													bg-slate-700
-													border border-slate-500 rounded-md
-													text-white text-base
-													leading-normal
-											"
-										/>
-									</div>
+                  <!-- Password -->
+                  <div class="accent-slate-800">
+                    <label for="password"
+                           class="
+                            block mb-1 text-sm
+                            font-bold tracking-[0.1em]
+                            text-slate-200
+                           ">
+                      ${t("password")}
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder="••••••••"
+                      required
+                      class="
+                        w-full px-2 py-3 mb-5
+                        bg-slate-700
+                        border border-slate-500 rounded-md
+                        text-white text-base
+                        leading-normal
+                      "
+                    />
+                  </div>
 
-									<!-- Submit -->
-									<button type="submit" 
-													class="
-														w-full mt-4 py-2
-														bg-slate-800
-														text-slate-200 text-base font-bold
-														border border-slate-600
-														tracking-[0.1em]
-														cursor-pointer
-														transition-all ease-in-out duration-200
-														hover:bg-slate-600 hover:border-slate-200
-														hover:translate-y-[1px]
-													">
-										${t("enter")}
-									</button>
+                  <!-- Submit -->
+                  <button type="submit" 
+                          class="
+                            w-full mt-4 py-2
+                            bg-slate-800
+                            text-slate-200 text-base font-bold
+                            border border-slate-600
+                            tracking-[0.1em]
+                            cursor-pointer
+                            transition-all ease-in-out duration-200
+                            hover:bg-slate-600 hover:border-slate-200
+                            hover:translate-y-[1px]
+                          ">
+                    ${t("enter")}
+                  </button>
 
-									<!-- Footer -->
-									<div class="mt-5 text-center underline decoration-slate-400">
-										<a class="hover:bg-blue-500" href="/register" data-nav>${t("to_signup")}</a>
-									</div>
+                  <!-- Footer -->
+                  <div class="mt-5 text-center underline decoration-slate-400">
+                    <a class="hover:bg-blue-500" href="/register" data-nav>${t("to_signup")}</a>
+                  </div>
 
-									<div class="mt-4 mb-8 text-center underline decoration-slate-400">
-										<a class="hover:bg-blue-500" href="/" data-nav>${t("home_return")}</a>
-									</div>
+                  <div class="mt-4 mb-8 text-center underline decoration-slate-400">
+                    <a class="hover:bg-blue-500" href="/" data-nav>${t("home_return")}</a>
+                  </div>
 
-									<div class="
-													mb-3
-													border-t border-slate-300
-													relative text-center
-											">
-										<span class="
-														relative -top-3
-														bg-slate-900
-														px-3
-														text-xs text-slate-200
-														tracking-[0.1em]
-														whitespace-nowrap
-													">
-											${t("other_login_methods")}
-										</span>
-									</div>
+                  <div class="
+                        mb-3
+                        border-t border-slate-300
+                        relative text-center
+                       ">
+                    <span class="
+                            relative -top-3
+                            bg-slate-900
+                            px-3
+                            text-xs text-slate-200
+                            tracking-[0.1em]
+                            whitespace-nowrap
+                          ">
+                      ${t("other_login_methods")}
+                    </span>
+                  </div>
 
-									<div class="flex flex-col items-center space-y-2">
-										<div id="google-btn"
-												class="
-														flex justify-center
-														hover:translate-y-[1px]
-												">
-										</div>
-										<p id="google-msg"
-											class="
-													mt-2.5 text-xs 
-													text-slate-500 text-center 
-													whitespace-pre-wrap
-										"></p>
-									</div>
+                  <div class="flex flex-col items-center space-y-2">
+                    <div id="google-btn"
+                         class="
+                          flex justify-center
+                          hover:translate-y-[1px]
+                         ">
+                    </div>
+                    <p id="google-msg"
+                       class="
+                        mt-2.5 text-xs 
+                        text-slate-500 text-center 
+                        whitespace-pre-wrap
+                    "></p>
+                  </div>
 
-									<p id="login-msg" 
-										class="
-												mt-3
-												text-xs text-slate-300 text-center
-												whitespace-pre-wrap
-									"></p>
-								</form>
-							</div>
-						</div>
-					`;
+                  <p id="login-msg" 
+                     class="
+                      mt-3
+                      text-xs text-slate-300 text-center
+                      whitespace-pre-wrap
+                  "></p>
+                </form>
+              </div>
+            </div>
+          `;
   };
 }
 
--- frontend/src/pages/register/index.ts
+++ frontend/src/pages/register/index.ts
@@ -22,199 +22,199 @@ class RegisterComponent implements Component {
   render = () => {
     return `
             <div class="
-										min-h-[calc(100dvh-64px)]
-										flex justify-center
-										px-1 py-2 md:py-6
-										overflow-y-auto
-										bg-[radial-gradient(circle_at_center,#1c1c1c,#0b0b0b)]
-								 ">
-							<div class="
-										w-[380px] px-8 pt-8 pb-5
-										my-auto
-										border-2 border-slate-500 rounded-2xl
-										shadow-[0_0_30px_rgba(0,0,0,0.8)]
-									 ">
-								<h2 class="
-											text-center text-slate-50 text-5xl
-											font-bold tracking-[0.1em]
-											mb-7
-										">
-									${t("signup")}
-								</h2>
+                   min-h-[calc(100dvh-64px)]
+                   flex justify-center
+                   px-1 py-2 md:py-6
+                   overflow-y-auto
+                   bg-[radial-gradient(circle_at_center,#1c1c1c,#0b0b0b)]
+                 ">
+              <div class="
+                     w-[380px] px-8 pt-8 pb-5
+                     my-auto
+                     border-2 border-slate-500 rounded-2xl
+                     shadow-[0_0_30px_rgba(0,0,0,0.8)]
+                   ">
+                <h2 class="
+                      text-center text-slate-50 text-5xl
+                      font-bold tracking-[0.1em]
+                      mb-7
+                    ">
+                  ${t("signup")}
+                </h2>
 
-								<form id="register_form" class="accent-slate-800">
+                <form id="register_form" class="accent-slate-800">
 								
-									<!-- Email -->
-									<div class="accent-slate-800">
-										<label for="email"
-													class="
-															block mb-1 text-sm
-															font-bold tracking-[0.1em]
-															text-slate-200
-													">
-											${t("email")}
-										</label>
-										<input
-											type="email"
-											id="email"
-											name="email"
-											placeholder="you@example.com"
-											required
-											class="
-													w-full px-2 py-3 mb-5
-													bg-slate-700
-													border border-slate-500 rounded-md
-													text-slate-200 text-base
-													leading-normal											
-											"
-										/>
-									</div>
+                  <!-- Email -->
+                  <div class="accent-slate-800">
+                    <label for="email"
+                           class="
+                            block mb-1 text-sm
+                            font-bold tracking-[0.1em]
+                            text-slate-200
+                           ">
+                      ${t("email")}
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="you@example.com"
+                      required
+                      class="
+                        w-full px-2 py-3 mb-5
+                        bg-slate-700
+                        border border-slate-500 rounded-md
+                        text-slate-200 text-base
+                        leading-normal											
+                      "
+                    />
+                  </div>
 
-									<div class="register-field">
-										<label for="username"
-													class="
-															block mb-1 text-sm
-															font-bold tracking-[0.1em]
-															text-slate-200
-													">
-											${t("username")}
-										</label>
-										<input
-											type="text"
-											id="username"
-											name="username"
-											placeholder="yourname"
-											required
-											class="
-													w-full px-2 py-3 mb-5
-													bg-slate-700
-													border border-slate-500 rounded-md
-													text-slate-200 text-base
-													leading-normal
-											"
-										/>
-									</div>
+                  <div class="register-field">
+                    <label for="username"
+                           class="
+                            block mb-1 text-sm
+                            font-bold tracking-[0.1em]
+                            text-slate-200
+                           ">
+                      ${t("username")}
+                    </label>
+                    <input
+                      type="text"
+                      id="username"
+                      name="username"
+                      placeholder="yourname"
+                      required
+                      class="
+                        w-full px-2 py-3 mb-5
+                        bg-slate-700
+                        border border-slate-500 rounded-md
+                        text-slate-200 text-base
+                        leading-normal
+                      "
+                    />
+                  </div>
 
-									<!-- Password -->
-									<div class="accent-slate-800">
-										<label for="password"
-														class="
-															block mb-1 text-sm
-															font-bold tracking-[0.1em]
-															text-slate-200
-														">
-											${t("password")}
-										</label>
-										<input
-											type="password"
-											id="password"
-											name="password"
-											placeholder="••••••••"
-											required
-											class="
-												w-full px-2 py-3 mb-5
-												bg-slate-700
-												border border-slate-500 rounded-md
-												text-slate-200 text-base
-												leading-normal
-											"
-										/>
-									</div>
+                  <!-- Password -->
+                  <div class="accent-slate-800">
+                    <label for="password"
+                           class="
+                            block mb-1 text-sm
+                            font-bold tracking-[0.1em]
+                            text-slate-200
+                           ">
+                      ${t("password")}
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder="••••••••"
+                      required
+                      class="
+                        w-full px-2 py-3 mb-5
+                        bg-slate-700
+                        border border-slate-500 rounded-md
+                        text-slate-200 text-base
+                        leading-normal
+                      "
+                    />
+                  </div>
 
-									<div class="register-field">
-										<label for="password_confirm"
-													class="
-															block mb-1 text-sm
-															font-bold tracking-[0.1em]
-															text-slate-200
-													">
-											${t("password_confirm")}
-										</label>
+                  <div class="register-field">
+                    <label for="password_confirm"
+                           class="
+                            block mb-1 text-sm
+                            font-bold tracking-[0.1em]
+                            text-slate-200
+                           ">
+                      ${t("password_confirm")}
+                    </label>
 
-										<input
-											type="password"
-											id="password_confirm"
-											name="password_confirm"
-											placeholder="••••••••"
-											required
-											class="
-													w-full px-2 py-3 mb-5
-													bg-slate-700
-													border border-slate-500 rounded-md
-													text-slate-200 text-base
-													leading-normal											
-											"
-										/>
-									</div>
+                    <input
+                      type="password"
+                      id="password_confirm"
+                      name="password_confirm"
+                      placeholder="••••••••"
+                      required
+                      class="
+                        w-full px-2 py-3 mb-5
+                        bg-slate-700
+                        border border-slate-500 rounded-md
+                        text-slate-200 text-base
+                        leading-normal											
+                      "
+                    />
+                  </div>
 										
-									<!-- Submit -->
-									<button id="register-submit" 
-													type="submit"
-													class="
-														w-full mt-4 py-2
-														bg-slate-800
-														text-slate-200 text-base font-bold
-														border border-slate-600
-														tracking-[0.1em]
-														cursor-pointer
-														transition-all ease-in-out duration-200
-														hover:bg-slate-600 hover:border-slate-200
-														hover:translate-y-[1px]
-													">
-										${t("register")}
-									</button>
+                  <!-- Submit -->
+                  <button id="register-submit" 
+                          type="submit"
+                          class="
+                            w-full mt-4 py-2
+                            bg-slate-800
+                            text-slate-200 text-base font-bold
+                            border border-slate-600
+                            tracking-[0.1em]
+                            cursor-pointer
+                            transition-all ease-in-out duration-200
+                            hover:bg-slate-600 hover:border-slate-200
+                            hover:translate-y-[1px]
+                          ">
+                    ${t("register")}
+                  </button>
 									
-									<!-- Footer -->
-									<div class="mt-5 text-center underline decoration-slate-400">
-										<a class="hover:bg-blue-500" href="/login" data-nav>${t("to_login")}</a>
-									</div>
+                  <!-- Footer -->
+                  <div class="mt-5 text-center underline decoration-slate-400">
+                    <a class="hover:bg-blue-500" href="/login" data-nav>${t("to_login")}</a>
+                  </div>
 
-									<div class="mt-4 mb-8 text-center underline decoration-slate-400">
-										<a class="hover:bg-blue-500" href="/" data-nav>${t("home_return")}</a>
-									</div>
+                  <div class="mt-4 mb-8 text-center underline decoration-slate-400">
+                    <a class="hover:bg-blue-500" href="/" data-nav>${t("home_return")}</a>
+                  </div>
 
-									<div class="
-													mb-3
-													border-t border-slate-300
-													relative text-center
-											">
-										<span class="
-														relative -top-3
-														bg-slate-900
-														px-3
-														text-xs text-slate-200
-														tracking-[0.1em]
-														whitespace-nowrap
-													">
-											${t("other_signup_methods")}
-										</span>
-									</div>
+                  <div class="
+                        mb-3
+                        border-t border-slate-300
+                        relative text-center
+                       ">
+                    <span class="
+                            relative -top-3
+                            bg-slate-900
+                            px-3
+                            text-xs text-slate-200
+                            tracking-[0.1em]
+                            whitespace-nowrap
+                          ">
+                      ${t("other_signup_methods")}
+                    </span>
+                  </div>
 
-									<div class="flex flex-col items-center">
-										<div id="google-btn-register" 
-												class="
-														flex justify-center
-														hover:translate-y-[1px]
-												">
-										</div>
-										<p id="google-msg"
-											class="
-													mt-2.5 text-xs
-													text-slate-500 text-center
-													whitespace-pre-wrap
-										"></p>
-									</div>
+                  <div class="flex flex-col items-center">
+                    <div id="google-btn-register" 
+                         class="
+                          flex justify-center
+                          hover:translate-y-[1px]
+                         ">
+                    </div>
+                    <p id="google-msg"
+                       class="
+                        mt-2.5 text-xs
+                        text-slate-500 text-center
+                        whitespace-pre-wrap
+                    "></p>
+                  </div>
 
-									<p id="register-msg"
-										class="
-												mt-3
-												text-xs text-slate-300 text-center
-												whitespace-pre-wrap	
-									"></p>
-								</form>
-							</div>
-						</div>
-        	`;
+                  <p id="register-msg"
+                     class="
+                      mt-3
+                      text-xs text-slate-300 text-center
+                      whitespace-pre-wrap	
+                  "></p>
+                </form>
+              </div>
+            </div>
+          `;
   };
 }
 
